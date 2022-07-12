054be45813351b8cdeb66aa9febbfcac48408686
Admin <rsd@gmail.com>
2022-07-12 18:33:26 +0300

    f string

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print('Hello World')
+first_name = input('Wahat is your first name?')
+last_name = input('What is your last name? ')
+full_name = f'Hello, {first_name} {last_name}'
+print(full_name.title())
