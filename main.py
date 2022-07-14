a270303ef7112b5b5fd950ac0f9e85e6f07d6b05
Igor <spanke46@gmail.com>
2022-07-14 16:50:00 +0300

    list. Add adn del + sort

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-
-
-
-names = ['vicky','igor','pasha','masha']
+names = ['Andrey','igor','pasha','masha']
 for guest in names:
     print (f'Hello {guest.title()}, I glad to see you')
 names.remove('pasha')
@@ -10,9 +7,20 @@ print('New guest list')
 for guest in names:
     print(guest.title())
 print('I bought the new table. I invited more guests! ')
-names.insert(0, 'Valera')
+names.insert(0, 'Nalera')
 names.insert(2, 'Nikita')
 names.append('Tolik')
 print('New guest list')
 for guest in names:
-    print(guest.title())
+    print((guest.title()))
+print('Number of guests ' + str(len(names)))
+del_guest = names.pop()
+del_guest = names.pop()
+del_guest = names.pop()
+del_guest = names.pop()
+print('On dinner invited only two guests, sorry')
+names.sort()
+for guest in names:
+    print((guest.title()))
+
+print('Nubmer of guests ' + str(len(names)))
