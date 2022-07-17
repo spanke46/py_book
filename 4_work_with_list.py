947f86f2873ca1317a8184c094613c1a6f45ef66
Admin <rsd@gmail.com>
2022-07-17 16:48:13 +0300

    4.11

--- 4_work_with_list.py
+++ 4_work_with_list.py
@@ -1,11 +1,8 @@
-#Работа с сегментами
-food = ['pizza', 'burger', 'cake', 'bread', 'sup','falafel', 'carrot cake', 'cannoli', 'ice cream']
-print('«The first three items in the list are ' + f'{food[0:3]}')
-#Вивести 3 итема с середин
-food = ['pizza', 'burger', 'cake', 'bread', 'sup','falafel', 'carrot cake', 'cannoli', 'ice cream']
-print('Three items from the middle of the list are ' + f'{food[3:6]}')
-#Вывести послдение 3 итема
-food = ['pizza', 'burger', 'cake', 'bread', 'sup','falafel', 'carrot cake', 'cannoli', 'ice cream']
-print('The last three items in the list are ' + f'{food[-3:]}')
-
-print(len(food))
+#Копия списка
+my_food = ['pizza', 'burger', 'cake', 'bread', 'sup']
+friend_food = my_food[:]
+for my_favorite in my_food:
+    print('This favorite my food ' + f'{my_favorite}')
+friend_food.append('salad')
+for friend_favorit_food in friend_food:
+    print('This favorite food my friends ' + f'{friend_favorit_food}')
