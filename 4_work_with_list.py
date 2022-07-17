86fd5316d0a48dc7eb238f853959a20b75a0da23
Admin <rsd@gmail.com>
2022-07-17 16:21:16 +0300

    4.10

--- 4_work_with_list.py
+++ 4_work_with_list.py
@@ -1,3 +1,11 @@
 #Работа с сегментами
 food = ['pizza', 'burger', 'cake', 'bread', 'sup','falafel', 'carrot cake', 'cannoli', 'ice cream']
 print('«The first three items in the list are ' + f'{food[0:3]}')
+#Вивести 3 итема с середин
+food = ['pizza', 'burger', 'cake', 'bread', 'sup','falafel', 'carrot cake', 'cannoli', 'ice cream']
+print('Three items from the middle of the list are ' + f'{food[3:6]}')
+#Вывести послдение 3 итема
+food = ['pizza', 'burger', 'cake', 'bread', 'sup','falafel', 'carrot cake', 'cannoli', 'ice cream']
+print('The last three items in the list are ' + f'{food[-3:]}')
+
+print(len(food))
