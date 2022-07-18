4f58b7fb1c2e70d1ad1c4aaca4b21436ba1a4b74
Igor <spanke46@gmail.com>
2022-07-18 14:29:20 +0300

    Cycle IF. Exercise 5.3-5.6

--- 5. Comands IF.py
+++ 5. Comands IF.py
@@ -1,56 +1,8 @@
-'''Проверка равенства и неравенства строк
-car= input('Enter name the car: ')
-if car=='bmw':
-    print('Yes')
+"""#Цвета 1
+color = input('Enter the name color: ')
+alien_color = ["green", "yellow","red"]
+if color.lower() in alien_color:
+    print("Player gets five points")
 else:
-    print('no')
-#Проверки с использованием функции lower().
-name= input('Enter name: ')
-if name.lower()=='lina':
-    print('Yes')
-else:
-    print('no')
-
-#Числовые проверки равенства
-print('Check ==')
-num_1 = input('Enter first number: ')
-num_2 = input('Enter second number: ')
-if num_1==num_2:
-    print('Yes')
-else:
-    print('No')
-
-#неравенства
-print('Check !=')
-num_1 = input('Enter first number: ')
-num_2 = input('Enter second number: ')
-if num_1!=num_2:
-    print('Yes')
-else:
-    print('No')
-
-#Проверки с ключевым словом and и or.
-in_user = input('Enter your name: ')
-in_pass = input('Enter your password: ')
-user = 'igor'
-passw = '123'
-if in_user.lower() == user and in_pass == passw:
-    print('Welcome to site')
-else:
-    print('Login or password is not correct' )
-
-#Проверка вхождения элемента в список
-in_guest= input('Enter your name: ')
-guests = ['igor', 'pasha','masha']
-if in_guest.lower() in guests:
-    print('Welcome')
-else:
-    print('You are not in list' )
-'''
-#Проверка отсутствия элемента в списке.
-ban_guests = ['igor', 'pasha','masha']
-guest= input('Enter your name: ')
-if guest.lower() not in ban_guests:
-    print('Welcome')
-else:
-    print('You are banned' )
+    print("Player doesn't  the points")
+"""
