9eaf5eee3c7a3c5fa090e708b9bc351596f4fe25
Igor <spanke46@gmail.com>
2022-07-19 13:57:07 +0300

    Cycle IF. example how I added new the topping in pizza

--- 5. Comands IF.py
+++ 5. Comands IF.py
@@ -1,44 +1,10 @@
-"""#Цвета 1-2
-#выберите цвет, как это было сделано в упражнении 5-3, и напишите цепочку if-else.
-#Если переменная содержит любое другое значение, выведите сообщение о том, что игрок только что заработал 10 очков.
-color = input('Enter the name color: ')
-alien_color = ["green", "yellow","red"]
-if color.lower() in alien_color:
-    print("Player gets five points")
-else:
-    print("Player doesn't  the points")
-"""
-"""#Цвета 3. преобразуйте цепочку if-else из упражнения 5-4 в цепочку if-elif-else.
+standard_toppings = ["mushrookms", "meat", "red peppers"]
+requested_toppings = []
+enter_topping=input("Enter name the topping: ")
+requested_toppings.append(enter_topping.lower())
 
-color = input('Enter the name color: ')
-if color.lower():
-    print("Player gets 5 points")
-elif color.lower() == "yellow":
-    print("Player gets 10 points")
-elif color.lower() == "red":
-    print("Player gets 15 points")
-else:
-    print("Player doesn't  the points")
-"""
-#5.6 Периоды жизни
-"""age = int(input("Enter age: "))
-if age < 2:
-    print("младенец")
-elif age== 2 or age==3:
-    print('малыш')
-elif age>=4 and age<13:
-    print("ребенок")
-elif age>=13 and age <20:
-    print("подросток")
-elif age>=20 and age<65:
-    print("Взрослый")
-else:
-    print("Пожилой")
-"""
-#5.7 Любимый фрукт
-favorite_fruits=["apple","chery","orange"]
-fruits=input("Enter the fruit: ")
-if fruits.lower() in favorite_fruits:
-    print("Yes, I really like " +f'{fruits}')
-else:
-    print("No")
+for requested_topping in requested_toppings:
+    if requested_topping in standard_toppings:
+        print("Added "+ requested_topping)
+    else:
+        print("Sorry, we don't have " + enter_topping)
