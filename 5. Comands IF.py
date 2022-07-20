76e13287e536cdf15da5a320686c0e3c76ed76e6
Igor <spanke46@gmail.com>
2022-07-20 16:24:14 +0300

    Cycle IF. task 5.10

--- 5. Comands IF.py
+++ 5. Comands IF.py
@@ -1,7 +1,24 @@
-"""""""#5.8
+"""#5.8
 list_users = ["admin","Erl","root","ted"]
 for users in list_users:
     if users=="admin":
         print("Hello admin , would you like to see a status report?")
     else:
         print("Hello " + users + ", thank you for logging in again")
+"""
+
+"""#5.9
+list_users = [""]
+for users in list_users:
+    if users=="":
+        print("We need to find some users!")
+"""
+#5.10
+current_users = ["lina","dima","denis","vera","geid"]
+new_users = ["lina","dima","vasya","liza","pit"]
+for users in new_users:
+    if users in current_users:
+        print("User " +users.title() + " already exists")
+    else:
+        print("Add new user " + users )
+
