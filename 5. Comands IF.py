f41607a271436811094a0079c7cf92d507d57b76
Igor <spanke46@gmail.com>
2022-07-21 09:42:01 +0300

    Cycle IF. task 5.11 completed

--- 5. Comands IF.py
+++ 5. Comands IF.py
@@ -13,7 +13,7 @@ for users in list_users:
     if users=="":
         print("We need to find some users!")
 """
-#5.10
+"""#5.10
 current_users = ["lina","dima","denis","vera","geid"]
 new_users = ["lina","dima","vasya","liza","pit"]
 for users in new_users:
@@ -21,4 +21,16 @@ for users in new_users:
         print("User " +users.title() + " already exists")
     else:
         print("Add new user " + users )
+"""
+#5.11 Порядковые числительные
 
+numbers = list(range(1,10))
+for number in numbers:
+    if number==1:
+        print("1st")
+    elif number==2:
+        print("2nd")
+    elif number==3:
+        print("3rd")
+    else:
+        print(str(number)+"th")
