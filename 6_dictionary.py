b9bbc2b6f5ef47ca092d443d70a20d18bde6fc99
Igor <spanke46@gmail.com>
2022-07-22 15:03:59 +0300

    Dictionary. Cycle "for" for dictionary

---
+++ 6_dictionary.py
@@ -0,0 +1,21 @@
+'''#6.1
+human_0 = {
+    'first_name': 'igor',
+    'last_name': 'titor',
+    'age': 27,
+    'city': 'Odessa'
+    }
+print(human_0['first_name'])
+print(human_0['last_name'])
+print(human_0['age'])
+print(human_0['city'])
+'''
+
+human_0 = {
+    'Igor': 'Python',
+    'Lina': 'C#',
+    'Vicky': 'Java',
+    'Pasha': 'C++'
+    }
+for name, language in human_0.items():
+    print(name.title() + "'s favorite language is " + language.title())
