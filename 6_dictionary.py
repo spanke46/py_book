82bd672b4289b5d313fad5ba79346027ec166ecd
Igor <spanke46@gmail.com>
2022-07-27 16:46:04 +0300

    Dictionary. тест

--- 6_dictionary.py
+++ 6_dictionary.py
@@ -1,10 +1,24 @@
-favorite_languages = {
- 'jen': ['python', 'ruby'],
- 'sarah': ['c'],
- 'edward': ['ruby', 'go'],
- 'phil': ['python', 'haskell'],
- }
-for name, languages in favorite_languages.items():
-    print("\n" + name.title() + "'s favorite languages are:")
-    for language in languages:
-        print(language.title())
+#6.1
+
+user_1 = {
+'first_name' : 'igor',
+'second_name' : 'titor',
+'age' : 25,
+'city' : 'odessa'
+}
+user_2 = {
+'first_name' : 'pasha',
+'second_name' : 'oslo',
+'age' : 33,
+'city' : 'Lviv'
+}
+user_3 = {
+'first_name' : 'dima',
+'second_name' : 'perl',
+'age' : 12,
+'city' : 'Dnepr'
+}
+people = [user_1,user_2,user_3]
+
+for user in people:
+    print(user)
