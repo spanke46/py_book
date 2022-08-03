fe1bb92d8c82284f7cb0aac4bbcbab194c1923f8
Admin <rsd@gmail.com>
2022-08-03 20:05:38 +0300

    7.Input. Task 7.5 final

--- 7. Input and while.py
+++ 7. Input and while.py
@@ -8,7 +8,8 @@ while True:
         print("Exit")
         break
 '''
-#7-5. Билеты в кино
+
+'''#7-5. Билеты в кино
 
 while True:
     age=int(input("Enter your age: "))
@@ -16,4 +17,23 @@ while True:
         print("Your ticket is free")
     elif age ==3 or age <= 11:
         print("Cost ticket 10$")
+    elif age > 12:
+        print("Cost ticket 15$")
         break
+'''
+
+#7.6. Три выхода
+
+while True:
+    age=input("Enter your age: ")
+    if age == 'q':
+        print("Exit")
+        break
+    age = int(age)
+    if age < 3 :
+        print("Your ticket is free")
+    elif age ==3 or age <= 11:
+        print("Cost ticket 10$")
+    elif age >= 12:
+        print("Cost ticket 15$")
+
