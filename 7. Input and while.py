65a44f14fb62b0f7ac96e510524430129e5110c9
Igor <spanke46@gmail.com>
2022-08-03 17:02:10 +0300

    7.Input. Task 7.4

--- 7. Input and while.py
+++ 7. Input and while.py
@@ -1,18 +1,11 @@
-'''#7.1Прокат машин
-car = input('Enter name car: ')
-print("Let me see if I can find you a " + car)
-'''
 
-'''#7.2 Заказ стола
-table = int(input('Please enter number person: '))
-if table > 8:
-    print("Please wait")
-else:
-    print("Table is ready!")
+
+'''# 7-4 Дополнения для пиццы
+while True:
+    m=input("Enter is topping name: ")
+    print("Your are topping "+ m + " add!")
+    if m == 'q':
+        print("Exit")
+        break
 '''
-#7.3 Числа, кратные 10
-num = int(input('Enter nummer: '))
-if num % 10 == 0:
-    print("кратное")
-else:
-    print("Не кратное")
+#7-5. Билеты в кино
