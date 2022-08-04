fe1692ecc30ee892f0cf3918ea707d85bfa98c60
Admin <rsd@gmail.com>
2022-08-04 21:00:14 +0300

    8.Function. Task 8.2

--- 8.functions.py
+++ 8.functions.py
@@ -1,6 +1,8 @@
 
 
-def display_message():
-    print("I now study the function! ")
 
-display_message()
+
+def favorite_book(book):
+    print(f"My favorite book is {book.title()}")
+
+favorite_book('fsdfsd')
