0b2615dc134469cc03daca92f50ccf3b3fb81c1b
Igor <spanke46@gmail.com>
2022-08-04 16:02:33 +0300

    7.Input. Task 7.10 vacation list

--- 7. Input and while.py
+++ 7. Input and while.py
@@ -1,39 +1,13 @@
 
-
-'''# 7-4 Дополнения для пиццы
-while True:
-    m=input("Enter is topping name: ")
-    print("Your are topping "+ m + " add!")
-    if m == 'q':
-        print("Exit")
-        break
-'''
-
-'''#7-5. Билеты в кино
-
-while True:
-    age=int(input("Enter your age: "))
-    if age < 3 :
-        print("Your ticket is free")
-    elif age ==3 or age <= 11:
-        print("Cost ticket 10$")
-    elif age > 12:
-        print("Cost ticket 15$")
-        break
-'''
-
-#7.6. Три выхода
-
-while True:
-    age=input("Enter your age: ")
-    if age == 'q':
-        print("Exit")
-        break
-    age = int(age)
-    if age < 3 :
-        print("Your ticket is free")
-    elif age ==3 or age <= 11:
-        print("Cost ticket 10$")
-    elif age >= 12:
-        print("Cost ticket 15$")
+list_vocation = []
+active = True
+while active:
+    questions= input('Where do you want to go vacation?')
+    if questions == 'q':
+        active=False
+    list_vocation.append(questions)
+print("Your list vacation:")
+list_vocation.remove('q')
+for sort_list in list_vocation:
+    print(sort_list.title())
 
