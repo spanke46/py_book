6d9f10cfaea14766e725cffaaf9610195e81723c
Admin <rsd@gmail.com>
2022-08-04 21:51:45 +0300

    8.Function. Task 8.3-8.5

--- 8.functions.py
+++ 8.functions.py
@@ -2,7 +2,31 @@
 
 
 
-def favorite_book(book):
-    print(f"My favorite book is {book.title()}")
 
-favorite_book('fsdfsd')
+
+'''#8.3 Футболка
+def make_shirt(size, text):
+    print(f"Your shirt have got size {size}, and this text {text}")
+
+#position argument
+make_shirt(54, 'Hello')
+
+#name argument
+make_shirt(text='Hi there!',size=45 )
+'''
+
+'''#8.4 Большие футболки
+def make_shirt(size="L", text="I love python"):
+    print(f"Your shirt have got size {size}, and this text {text}")
+
+make_shirt(text="I don't love python")
+'''
+def describe_city(city="Kyiv", county="Ukraine"):
+    print(f"{city.title()} is in  {county.title()}")
+describe_city()
+describe_city(city="Odessa")
+describe_city(city="Lviv")
+describe_city("dnepr")
+
+
+
