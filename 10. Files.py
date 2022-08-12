25e19917f829027522f25add076dd07c18ae0c78
Admin <rsd@gmail.com>
2022-08-12 20:04:28 +0300

    example2. dea + count words

--- 10. Files.py
+++ 10. Files.py
@@ -3,9 +3,15 @@
 
 
 #10.4
-file_path = 'c:\py\pi.txt'
-with open(file_path, encoding='utf-8') as f_write:
-    content = f_write.read()
-word = content.split()
-num_words = len(word)
-print(num_words)
+def count_words(file_path):
+    try:
+        with open(file_path, encoding='utf-8') as f_write:
+            content = f_write.read()
+            word = content.split()
+            num_words = len(word)
+            print(num_words)
+    except FileNotFoundError:
+        print('File not found')
+
+file_path = input('Enter path: ')
+count_words(file_path)
