466193c3e9c365a2c7935020293f143de1ad25d1
Admin <rsd@gmail.com>
2022-08-15 20:32:18 +0300

    task 9.1

--- 9.Class.py
+++ 9.Class.py
@@ -1,7 +1,22 @@
 
-
+#9.1
 class Restaurant():
     def  __init__ (self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
-    def describe_restaurant():
+        self.door = 'ав'
+    def describe_restaurant(self):
+        print("Добро пожаловать в наш ресторан  " + self.restaurant_name + " " + self.cuisine_type + " кухни"  )
+
+    def open_restaurant(self, s_door):
+        self.door = s_door
+        print('Ресторан ' + s_door)
+
+
+
+
+my_rest = Restaurant('Яхта', 'Европйской')
+
+my_rest.describe_restaurant()
+my_rest.open_restaurant('открыт')
+
